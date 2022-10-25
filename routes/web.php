b17e2a494e711509529706a989180907945060fb
thongagl <dinh_cong_thong@allgrow-labo.jp>
2022-10-25 10:55:22 +0700

    tset route

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('thong', function () {
+    return 'thanh cong roi nha';
+});
 Route::get('/', function () {
     return view('welcome');
 })->middleware(['verify.shopify'])->name('home');
